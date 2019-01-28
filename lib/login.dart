acdf8c0686f062b9b2dd2f9d941cd0c55b050bf6
BartekK <bartek.kesicki25@gmail.com>
2019-01-28 20:34:01 +0100

    removed default counter feature, added title to main page

---
+++ lib/login.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(child:
+            Stack(children: <Widget>[
+              Container(
+                padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                child: Text(
+                    "My",
+                  style: TextStyle(
+                      fontSize : 80.0, fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(15.0, 175.0, 0.0, 0.0),
+                child: Text(
+                  "Expenses",
+                  style: TextStyle(
+                      fontSize : 80.0, fontWeight: FontWeight.bold
+                  ),
+                ),
+              )
+            ],),)
+        ],
+      ),// This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
