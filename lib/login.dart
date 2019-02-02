e489a6b79eb22a6acc80d0ee50ee18c1d8437eff
BartekK <bartek.kesicki25@gmail.com>
2019-02-02 12:01:35 +0100

    finish login page controls

--- lib/login.dart
+++ lib/login.dart
@@ -6,11 +6,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My Expenses',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'My Expenses'),
     );
   }
 }
@@ -77,6 +77,72 @@ class _MyHomePageState extends State<MyHomePage> {
                       )
                   ),
                   obscureText: true,
+                ),
+                SizedBox(height: 5.0),
+                Container(
+                  alignment: Alignment(1.0, 0.0),
+                  padding: EdgeInsets.only(top: 15, left: 20),
+                  child: InkWell(
+                    child: Text(
+                        "Forgot Password",
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Montserrat',
+                            decoration: TextDecoration.underline),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                Container(
+                  height: 50,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(25.0),
+                    shadowColor: Colors.lightGreen,
+                    color: Colors.green,
+                    elevation: 7.0,
+                    child: GestureDetector(
+                      onTap: () {
+                        //todo login button
+                      },
+                      child: Center(
+                        child: Text(
+                          "LOGIN",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "Montserrat"
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Container(
+                  height: 50.0,
+                  color: Colors.transparent,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        style: BorderStyle.solid,
+                        width: 1.0
+                      ),
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    child:
+                    Center(
+                      child: Text(
+                        "SIGN UP",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Montserrat"
+                        ),
+                      ),
+                    ),
+                  ),
                 )
               ],
             ),
