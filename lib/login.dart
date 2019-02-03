b61c0d6b35b15e12d96d8e291236d13eab5018a4
BartekK <bartek.kesicki25@gmail.com>
2019-02-03 19:02:55 +0100

    refactor code

--- lib/login.dart
+++ lib/login.dart
@@ -32,123 +32,117 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
         children: <Widget>[
           Container(
-            child:
-            Stack(children: <Widget>[
-              Container(
-                padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
-                child: Text(
-                    "My",
-                  style: TextStyle(
-                      fontSize : 80.0, fontWeight: FontWeight.bold
-                  ),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.fromLTRB(15.0, 175.0, 0.0, 0.0),
-                child: Text(
-                  "Expenses",
-                  style: TextStyle(
-                      fontSize : 80.0, fontWeight: FontWeight.bold
-                  ),
-                ),
-              ),
-            ],),),
+            child: createHeader()
+          ),
           Container(
             padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
             child: Column(
               children: <Widget>[
                 TextField(
-                  decoration: InputDecoration(
-                      labelText: "EMAIL",
-                      labelStyle: TextStyle(
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey
-                      )
-                  ),
+                  decoration: createLoginTextFieldDecoration("EMAIL"),
                 ),
                 TextField(
-                  decoration: InputDecoration(
-                      labelText: "PASSWORD",
-                      labelStyle: TextStyle(
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey
-                      )
-                  ),
+                  decoration: createLoginTextFieldDecoration("PASSWORD"),
                   obscureText: true,
                 ),
-                SizedBox(height: 5.0),
+                createSizedBox(5.0),
                 Container(
                   alignment: Alignment(1.0, 0.0),
                   padding: EdgeInsets.only(top: 15, left: 20),
                   child: InkWell(
-                    child: Text(
-                        "Forgot Password",
-                        style: TextStyle(
-                            color: Colors.green,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Montserrat',
-                            decoration: TextDecoration.underline),
-                    ),
+                    child: createText("Forgot Password", createForgotPasswordTextStyle())),
                   ),
-                ),
-                SizedBox(height: 30),
-                Container(
-                  height: 50,
-                  child: Material(
-                    borderRadius: BorderRadius.circular(25.0),
-                    shadowColor: Colors.lightGreen,
-                    color: Colors.green,
-                    elevation: 7.0,
-                    child: GestureDetector(
-                      onTap: () {
-                        //todo login button
-                      },
-                      child: Center(
-                        child: Text(
-                          "LOGIN",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: "Montserrat"
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20.0),
-                Container(
-                  height: 50.0,
-                  color: Colors.transparent,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                        style: BorderStyle.solid,
-                        width: 1.0
-                      ),
-                      color: Colors.transparent,
-                      borderRadius: BorderRadius.circular(25.0),
-                    ),
-                    child:
-                    Center(
-                      child: Text(
-                        "SIGN UP",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontFamily: "Montserrat"
-                        ),
-                      ),
-                    ),
-                  ),
-                )
+                createSizedBox(30.0),
+                createLoginButton(),
+                createSizedBox(20.0),
+                createSignUpButton()
               ],
             ),
           )
         ],
       ),// This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Stack createHeader() {
+    return new Stack(children: <Widget>[
+      createTopLabelsContainer(createText("My", createTitleTextStyle()), EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0)),
+      createTopLabelsContainer(createText("Expenses", createTitleTextStyle()), EdgeInsets.fromLTRB(15.0, 175.0, 0.0, 0.0))
+    ]);
+  }
+
+  Container createLoginButton() {
+    return new Container(
+      height: 50,
+      child: Material(
+        borderRadius: BorderRadius.circular(25.0),
+        shadowColor: Colors.lightGreen,
+        color: Colors.green,
+        elevation: 7.0,
+        child: GestureDetector(
+          onTap: () {
+            //todo login button
+          },
+          child: Center(
+            child: createText("LOGIN", createLoginButtonTextStyle()),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container createSignUpButton() {
+    return new Container(
+      height: 50.0,
+      color: Colors.transparent,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+              color: Colors.black,
+              style: BorderStyle.solid,
+              width: 1.0
+          ),
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(25.0),
+        ),
+        child: Center(
+            child: createText("SIGN UP", TextStyle(fontWeight: FontWeight.bold, fontFamily: "Montserrat"))
+        ),
+      ),
+    );
+  }
+
+  Container createTopLabelsContainer(Text text, EdgeInsets insets) {
+    return new Container(
+      padding: insets,
+      child: text,
+    );
+  }
+
+  SizedBox createSizedBox(double height) {
+    return new SizedBox(height: height,);
+  }
+
+  Text createText(String label, TextStyle textStyle) {
+    return new Text(label, style: textStyle,);
+  }
+
+  TextStyle createTitleTextStyle() {
+    return new TextStyle(fontSize : 80.0, fontWeight: FontWeight.bold);
+  }
+
+  TextStyle createLoginButtonTextStyle() {
+    return new TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontFamily: "Montserrat");
+  }
+
+  InputDecoration createLoginTextFieldDecoration(String labelText) {
+    return new InputDecoration(
+        labelText: labelText,
+        labelStyle: TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.bold, color: Colors.grey)
+    );
+  }
+
+  TextStyle createForgotPasswordTextStyle() {
+    return new TextStyle(color: Colors.green, fontWeight: FontWeight.bold, fontFamily: 'Montserrat', decoration: TextDecoration.underline);
   }
 }
