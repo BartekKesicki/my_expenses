900a2be53feb2a3674e4dbc91b642fb3b1e2894c
BartekK <bartek.kesicki25@gmail.com>
2019-10-10 16:50:12 +0200

    attached invoking events to login and register page

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -8,6 +8,9 @@ import 'package:my_expenses/home_page/home_page.dart';
 import 'package:my_expenses/login/login_bloc.dart';
 import 'package:my_expenses/register/register_personal_data_page.dart';
 
+import 'login_event.dart';
+import 'login_state.dart';
+
 class LoginPage extends StatelessWidget  {
 
   final _userTextController = TextEditingController();
@@ -20,69 +23,80 @@ class LoginPage extends StatelessWidget  {
     return SingleChildScrollView(
         child: BlocProvider(
           builder: (BuildContext context) => loginBloc,
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                createHeader(context),
-            Container(
-                padding: EdgeInsets.only(
-                    left: AppDimens.loginContainerSideMargin,
-                    right: AppDimens.loginContainerSideMargin),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      TextField(
-                        decoration: AppStyles.createTextFieldDecoration(AppStrings.email),
-                        controller: _userTextController,
-                        onChanged: (value) {
-                          //todo invoke event
-                        },
-                      ),
-                      TextField(
-                        decoration: AppStyles.createTextFieldDecoration(AppStrings.password),
-                        controller: _passwordTextController,
-                        obscureText: true,
-                        onChanged: (value) {
-                          //todo invoke change event
-                        },
-                      ),
-                      Container(
-                        alignment: Alignment(1.0, 0.0),
-                        padding: EdgeInsets.only(
-                            top: AppDimens.loginLowerContainerTopMargin,
-                            left: AppDimens.loginContainerSideMargin,
-                            bottom: AppDimens.loginBottomContainerMargin),
-                        child: InkWell(
-                            child: AppWidgets.createText(
-                                AppStrings.forgotPassword,
-                                AppStyles.createHyperLinkTextStyle())),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                            top: AppDimens.loginTopContainerTopMargin,
-                            bottom: AppDimens.loginBottomContainerMargin),
-                        child: Container(
-                            height: AppDimens.appRaisedButtonHeight,
-                            child: RaisedButton(
-                              shape: new RoundedRectangleBorder(
-                                  borderRadius: new BorderRadius.circular(
-                                      AppDimens.appRaisedButtonCornerRadius)),
-                              elevation: AppDimens.appRaisedButtonElevation,
-                              color: Colors.green,
-                              onPressed: () {
-                                //todo invoke event
-                              },
-                              child: Center(
-                                  child: AppWidgets.createText(AppStrings.login,
-                                      AppStyles.createButtonTextStyle())),
-                            ),),
-                      ),
-                      createSignUpButton(context),
-                    ]))
-          ]),
+          child: BlocBuilder(
+            bloc: loginBloc,
+            builder: (BuildContext context, LoginState loginState) {
+              //todo return other widgets
+              return buildMainLoginWidget(context, loginState);
+          }),
         ));
+  }
+
+  Column buildMainLoginWidget(BuildContext context, InitialLoginState loginState) {
+    return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              createHeader(context),
+          Container(
+              padding: EdgeInsets.only(
+                  left: AppDimens.loginContainerSideMargin,
+                  right: AppDimens.loginContainerSideMargin),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    TextField(
+                      decoration: AppStyles.createTextFieldDecoration(AppStrings.email, loginState.usernameErrorMessage),
+                      controller: _userTextController,
+                      onChanged: (value) {
+                        final loginBloc = BlocProvider.of<LoginBloc>(context);
+                        loginBloc.dispatch(ValidateLoginEvent(_userTextController.text, _passwordTextController.text));
+                      },
+                    ),
+                    TextField(
+                      decoration: AppStyles.createTextFieldDecoration(AppStrings.password, loginState.passwordErrorMessage),
+                      controller: _passwordTextController,
+                      obscureText: true,
+                      onChanged: (value) {
+                        final loginBloc = BlocProvider.of<LoginBloc>(context);
+                        loginBloc.dispatch(ValidateLoginEvent(_userTextController.text, _passwordTextController.text));
+                      },
+                    ),
+                    Container(
+                      alignment: Alignment(1.0, 0.0),
+                      padding: EdgeInsets.only(
+                          top: AppDimens.loginLowerContainerTopMargin,
+                          left: AppDimens.loginContainerSideMargin,
+                          bottom: AppDimens.loginBottomContainerMargin),
+                      child: InkWell(
+                          child: AppWidgets.createText(
+                              AppStrings.forgotPassword,
+                              AppStyles.createHyperLinkTextStyle())),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                          top: AppDimens.loginTopContainerTopMargin,
+                          bottom: AppDimens.loginBottomContainerMargin),
+                      child: Container(
+                          height: AppDimens.appRaisedButtonHeight,
+                          child: RaisedButton(
+                            shape: new RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(
+                                    AppDimens.appRaisedButtonCornerRadius)),
+                            elevation: AppDimens.appRaisedButtonElevation,
+                            color: Colors.green,
+                            onPressed: () {
+                              //todo invoke event
+                            },
+                            child: Center(
+                                child: AppWidgets.createText(AppStrings.login,
+                                    AppStyles.createButtonTextStyle())),
+                          ),),
+                    ),
+                    createSignUpButton(context),
+                  ]))
+        ]);
   }
 
   Widget createHeader(BuildContext context) {
--- lib/register/register_personal_data_page.dart
+++ lib/register/register_personal_data_page.dart
@@ -5,6 +5,8 @@ import 'package:my_expenses/app_properties/app_strings.dart';
 import 'package:my_expenses/app_properties/app_styles.dart';
 import 'package:my_expenses/app_properties/app_widgets.dart';
 import 'package:my_expenses/register/register_personal_data_bloc.dart';
+import 'package:my_expenses/register/register_personal_data_event.dart';
+import 'package:my_expenses/register/register_personal_data_state.dart';
 
 class RegisterPersonalDataPage extends StatelessWidget {
 
@@ -21,65 +23,72 @@ class RegisterPersonalDataPage extends StatelessWidget {
         child: Container(
           height: MediaQuery.of(context).size.height,
           child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  AppWidgets.createTopLabelsContainer(AppWidgets.createText(AppStrings.personalData, AppStyles.createTitleTextStyle()), EdgeInsets.all(AppDimens.containerSideMargin)),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(left: AppDimens.containerSideMargin, right: AppDimens.containerSideMargin),
-                        child: TextField(
-                          controller: _userTextController,
-                          decoration: AppStyles.createTextFieldDecoration(AppStrings.login),
-                          onChanged: (value) {
-                            //todo invoke on change event
-                          },
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: AppDimens.containerSideMargin, right: AppDimens.containerSideMargin),
-                        child: TextField(
-                          controller: _passwordTextController,
-                          decoration: AppStyles.createTextFieldDecoration(AppStrings.password),
-                          obscureText: true,
-                          onChanged: (value) {
-                            //todo invoke on change event
-                          },
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: AppDimens.containerSideMargin, right: AppDimens.containerSideMargin),
-                        child: TextField(
-                          controller: _confirmPasswordTextController,
-                          decoration: AppStyles.createTextFieldDecoration(AppStrings.confirmPassword),
-                          obscureText: true,
-                          onChanged: (value) {
-                            //todo invoke onchange event
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                  //todo submit form
-                  Padding(
-                    padding: EdgeInsets.only(left: AppDimens.containerSideMargin, right: AppDimens.containerSideMargin, top: AppDimens.containerTopMargin),
-                    child: AppWidgets.createSubmitButton(() {
-                      //todo submit button
-                    }, AppWidgets.createText(AppStrings.next, AppStyles.createButtonTextStyle()))
-                  )
-                ],
-              )
+              child: BlocBuilder(
+                bloc: registerBloc,
+                  builder : (BuildContext context, RegisterPersonalDataState registerPersonalDataState) {
+                  //todo return other widgets
+                    return buildMainRegisterForm(context, registerPersonalDataState);
+              })
           ),
         ),
       ),
     );
   }
 
-  submitForm() {
-
+  Column buildMainRegisterForm(BuildContext context, InitialRegisterPersonalDataState initialRegisterPersonalDataState) {
+    return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                AppWidgets.createTopLabelsContainer(AppWidgets.createText(AppStrings.personalData, AppStyles.createTitleTextStyle()), EdgeInsets.all(AppDimens.containerSideMargin)),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(left: AppDimens.containerSideMargin, right: AppDimens.containerSideMargin),
+                      child: TextField(
+                        controller: _userTextController,
+                        decoration: AppStyles.createTextFieldDecoration(AppStrings.login, initialRegisterPersonalDataState.usernameErrorMessage),
+                        onChanged: (value) {
+                          final registerBloc = BlocProvider.of<RegisterPersonalDataBloc>(context);
+                          registerBloc.dispatch(ValidateRegisterPersonalDataEvent(_userTextController.text, _passwordTextController.text, _confirmPasswordTextController.text));
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: AppDimens.containerSideMargin, right: AppDimens.containerSideMargin),
+                      child: TextField(
+                        controller: _passwordTextController,
+                        decoration: AppStyles.createTextFieldDecoration(AppStrings.password, initialRegisterPersonalDataState.passwordErrorMessage),
+                        obscureText: true,
+                        onChanged: (value) {
+                          final registerBloc = BlocProvider.of<RegisterPersonalDataBloc>(context);
+                          registerBloc.dispatch(ValidateRegisterPersonalDataEvent(_userTextController.text, _passwordTextController.text, _confirmPasswordTextController.text));
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: AppDimens.containerSideMargin, right: AppDimens.containerSideMargin),
+                      child: TextField(
+                        controller: _confirmPasswordTextController,
+                        decoration: AppStyles.createTextFieldDecoration(AppStrings.confirmPassword, initialRegisterPersonalDataState.confirmPasswordErrorMessage),
+                        obscureText: true,
+                        onChanged: (value) {
+                          final registerBloc = BlocProvider.of<RegisterPersonalDataBloc>(context);
+                          registerBloc.dispatch(ValidateRegisterPersonalDataEvent(_userTextController.text, _passwordTextController.text, _confirmPasswordTextController.text));
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: AppDimens.containerSideMargin, right: AppDimens.containerSideMargin, top: AppDimens.containerTopMargin),
+                  child: AppWidgets.createSubmitButton(() {
+                    //todo submit button
+                  }, AppWidgets.createText(AppStrings.next, AppStyles.createButtonTextStyle()))
+                )
+              ],
+            );
   }
 }
