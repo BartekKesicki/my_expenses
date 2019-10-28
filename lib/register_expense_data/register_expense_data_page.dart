349eafa9f7e920922a2e270f4d9e1b17f636bebd
BartekK <bartek.kesicki25@gmail.com>
2019-10-28 14:25:59 +0100

    added scaffold to register expense data page

--- lib/register_expense_data/register_expense_data_page.dart
+++ lib/register_expense_data/register_expense_data_page.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_expenses/register_expense_data/register_expense_data_bloc.dart';
+import 'package:my_expenses/register_expense_data/register_expense_data_state.dart';
 
 class RegisterExpenseDataPage extends StatelessWidget {
 
+  final _registerBloc = RegisterExpenseDataBloc();
+
   @override
   Widget build(BuildContext context) {
-    // TODO: build a page
-    return Container();
+    return Scaffold(
+      body: BlocProvider(
+        builder: (BuildContext context) => _registerBloc,
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: SingleChildScrollView(
+              child: BlocBuilder(
+                  bloc: _registerBloc,
+                  builder : (BuildContext context, RegisterExpenseDataState registerExpenseDataState) {
+                    //todo fill with forms
+                  })
+          ),
+        ),
+      ),
+    );
   }
 
 }
