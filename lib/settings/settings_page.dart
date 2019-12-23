d95465b28276f4cd09d416c736c6fd865e0746f8
BartekK <bartek.kesicki25@gmail.com>
2019-12-23 09:04:27 +0100

    added bloc to settings page

--- lib/settings/settings_page.dart
+++ lib/settings/settings_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_expenses/app_properties/app_strings.dart';
 import 'package:my_expenses/app_properties/app_styles.dart';
 import 'package:my_expenses/app_properties/app_widgets.dart';
+import 'package:my_expenses/settings/settings_bloc.dart';
+import 'package:my_expenses/settings/settings_state.dart';
 
 class SettingsPage extends StatefulWidget {
   SettingsPage({Key key, this.title}) : super(key: key);
@@ -13,14 +16,25 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+
+  final _settingsBloc = SettingsPageBloc();
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: AppWidgets.createSubmitButton(() {
-        showAlertDialog();
-      },
-          AppWidgets.createText(
-              AppStrings.logout, AppStyles.createButtonTextStyle())),
+    return BlocProvider(
+      builder: (BuildContext context) => _settingsBloc,
+      child: BlocBuilder(
+          bloc: _settingsBloc,
+          builder: (BuildContext context, SettingsState settingsState) {
+            //todo handle other states
+            return Center(
+              child: AppWidgets.createSubmitButton(() {
+                showAlertDialog();
+              },
+                  AppWidgets.createText(
+                      AppStrings.logout, AppStyles.createButtonTextStyle())),
+            );
+          }),
     );
   }
 
